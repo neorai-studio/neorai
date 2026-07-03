313e8f65bcbf5dc04988c6efc929dfa5470b1661
NoaGrundland <noagrun.office@gmail.com>
2026-07-04 02:15:49 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   base: "/",
   plugins: [
     react({
@@ -11,4 +11,4 @@ export default defineConfig(({ command }) => ({
       },
     }),
   ],
-}));
+});
