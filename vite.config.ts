4b82fe4d204fed519857efb38fb9cf746c8aef80
NoaGrundland <noagrun.office@gmail.com>
2026-07-04 02:07:59 +0300

    FIX

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/neorai/",
+  base: "/",
   plugins: [
     react({
       babel: {
